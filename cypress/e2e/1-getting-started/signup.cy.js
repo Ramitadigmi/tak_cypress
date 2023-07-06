2410e804279dcf652a690795437f7ca6cd69c563
ram <ramita.digmi01@gmail.com>
2023-07-06 16:42:10 +0700

    oke

--- cypress/e2e/1-getting-started/signup.cy.js
+++ cypress/e2e/1-getting-started/signup.cy.js
@@ -1,5 +1,5 @@
 describe('Signup Azurewebsites', () => {
-  it('Succes Signup', () => { //testcasepositive
+  it.skip('Succes Signup', () => { //testcasepositive
     cy.visit('https://itera-qa.azurewebsites.net/')
     cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
     cy.url().should('include', 'https://itera-qa.azurewebsites.net/UserRegister/NewUser')
@@ -12,5 +12,31 @@ describe('Signup Azurewebsites', () => {
     cy.get('#ConfirmPassword').type('ramita12345')
     cy.get('#submit').click()
     cy.get('.label-success').should('have.text', 'Registration Successful')
+  })
+
+  it.skip('Failed Signup with same input field', () => { //testcasenegative1
+    cy.visit('https://itera-qa.azurewebsites.net/')
+    cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
+    cy.url().should('include', 'https://itera-qa.azurewebsites.net/UserRegister/NewUser')
+    cy.get('#FirstName').type('ramita')
+    cy.get('#Surname').type('digmi')
+    cy.get('#E_post').type('ramita.digmi01@gmail.com')
+    cy.get('#Mobile').type('088822227777')
+    cy.get('#Username').type('ramitaD1')
+    cy.get('#Password').type('ramita12345')
+    cy.get('#ConfirmPassword').type('ramita12345')
+    cy.get('#submit').click()
+    cy.get('.label-danger').should('have.text', 'Username already exist')
+  })
+
+  it('Failed Signup with no input field', () => { //testcasenegative2
+    cy.visit('https://itera-qa.azurewebsites.net/')
+    cy.get('.form-inline > .navbar-nav > :nth-child(1) > .nav-link').click()
+    cy.url().should('include', 'https://itera-qa.azurewebsites.net/UserRegister/NewUser')
+    cy.get('#submit').click()
+    cy.get('#FirstName-error').should('have.text', 'Please enter first name')
+    cy.get('#Surname-error').should('have.text','Please enter surname')
+    cy.get('#Username-error').should('have.text','Please enter username')
+    cy.get('#Password-error').should('have.text','Please enter password')
   })
 })
